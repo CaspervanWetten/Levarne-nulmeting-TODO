4dc25fa99c7b0e720f61913d94a2cff81fd5094d
Casper van Wetten <c.o.vanwetten@gmail.com>
2024-04-21 13:00:19 +0200

    Initial commit, finished project
    Added TODO getting from backend API, API key given manually in form;

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,110 @@
+<script setup lang="ts">
+const globalStore = useGlobalStore();
+</script>
+
 <template>
   <div class="page-wrapper">
     <div class="element-wrapper">
       <div class="element-body">
-        <h1>Welkom bij de Levarne Nulmeting</h1>
-        <LevButton @clicked="increment">Count me! {{ cnt }}</LevButton>
-        <br>
-        <br>
-        <NuxtLink to="/about">Over ons</NuxtLink>
+        <div class="header">
+          <h1>TODO Apps</h1>
+        </div>
+        <div class="table">
+          <table>
+            <thead>
+              <tr>
+                <th>Task</th>
+                <th>Assignee</th>
+                <th>Due date</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="todo in globalStore.todos" :key="todo.id">
+                <td>{{ todo.description }}</td>
+                <td>{{ todo.assignee }}</td>
+                <td>
+                  {{ new Date(todo.dueDateTime).toLocaleString("en-US") }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="button-wrapper">
+          <button @click="globalStore.getTodo(key)">Get another TODO</button>
+        </div>
+        <div class="form-wrapper">
+          <input
+            type="text"
+            class="key-input"
+            placeholder="enter API key"
+            v-model="key"
+          />
+        </div>
+        <div v-if="globalStore.rightKey" class="wrong-key">
+          <p>
+            dat is de verkeerde API key!
+            <br />
+            Probeer het nog eens :)
+          </p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
-const cnt = ref(0);
-
-function increment() {
-  cnt.value += 1;
+<style>
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background-color: #333;
+  color: #fff;
+  text-align: center;
 }
-</script>
+
+h1 {
+  margin-top: 80px;
+  font-size: 65px;
+}
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 20px;
+}
+
+th,
+td {
+  padding: 8px;
+  text-align: left;
+  border-bottom: 1px solid #ddd;
+}
+
+th {
+  background-color: #222;
+  color: #fff;
+}
+
+tr:hover {
+  background-color: #444;
+}
+
+button {
+  background-color: #006400;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin-top: 20px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #008000;
+}
+</style>
--- stores/global.ts
+++ stores/global.ts
@@ -1,12 +1,52 @@
+import { ref, reactive } from "vue";
+import { defineStore } from "pinia";
+
 export const useGlobalStore = defineStore("globalStore", () => {
   const loading = ref(false);
+  const rightKey = ref(false);
+  const todos = reactive([]);
+  todos.length = 0;
 
   const startLoader = (): void => {
     loading.value = true;
-  }
+  };
+  // const [function name] = (params): [return type] => {function}
   const stopLoader = (): void => {
     loading.value = false;
-  }
+  };
+  const checkExistence = (id: string): boolean => {
+    todos.forEach((todo) => {
+      if (todo.id === id) {
+        return true;
+      }
+    });
+    return false;
+  };
+  const getTodo = async (key: string) => {
+    loading.value = true;
+    let response;
+    let data;
+    try {
+      response = await fetch(
+        "https://86a4h9y007.execute-api.eu-west-1.amazonaws.com/development/nulmeting/todo",
+        { headers: { "x-api-key": key } }
+      );
+      rightKey.value = false;
+      data = await response.json();
+      if (checkExistence(data.todo.id)) {
+        loading.value = false;
+        return;
+      }
+      todos.push(data.todo);
+      loading.value = false;
+    } catch (error) {
+      console.log(error);
+      rightKey.value = true;
+      loading.value = false;
+      console.log(rightKey.value)
+      return;
+    }
+  };
 
-  return { loading, startLoader, stopLoader };
+  return { loading, todos, rightKey, startLoader, stopLoader, getTodo };
 });
